2cd204c58b90fbb8d8915b582580c0a5c2dcfc00
noriyuki-sasagawa <sasanori.0126@gmail.com>
2019-11-18 12:29:37 +0900

    ツイート用のページ追加

--- page/page.go
+++ page/page.go
@@ -6,6 +6,12 @@ import (
 	"github.com/labstack/echo"
 )
 
+// Index index.html
 func Index(c echo.Context) error {
 	return c.Render(http.StatusOK, "index.html", nil)
+}
+
+// Tweet tweet.html
+func Tweet(c echo.Context) error {
+    return c.Render(http.StatusOK , "tweet.html", nil)
 }
